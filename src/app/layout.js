3e9cb322fa4b91eef45b743e464354af335a7517
wigiart <wigiart8@gmail.com>
2024-11-29 14:50:49 +0530

    Update Google AdSense script formatting

--- src/app/layout.js
+++ src/app/layout.js
@@ -14,12 +14,8 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <head>
-        <Script
-          async
-          src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-9186108355380062"
-          crossOrigin="anonymous"
-          strategy="afterInteractive"
-        />
+      <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-9186108355380062"
+     crossorigin="anonymous"></script>
         <link href="https://fonts.googleapis.com/css2?family=Dancing+Script:wght@400;700&display=swap" rel="stylesheet" />
       </head>
       <body className={inter.className}>{children}</body>
