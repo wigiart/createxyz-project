7d0cf99c6b8379b07e2961a0a94e737be1d922f8
wigiart <wigiart8@gmail.com>
2024-11-29 14:45:14 +0530

    Update Google AdSense implementation using Next.js Script component

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,5 +1,6 @@
 import { Inter } from 'next/font/google'
 import './globals.css'
+import Script from 'next/script'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -13,7 +14,12 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <head>
-        <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-9186108355380062" crossorigin="anonymous"></script>
+        <Script
+          async
+          src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-9186108355380062"
+          crossOrigin="anonymous"
+          strategy="afterInteractive"
+        />
         <link href="https://fonts.googleapis.com/css2?family=Dancing+Script:wght@400;700&display=swap" rel="stylesheet" />
       </head>
       <body className={inter.className}>{children}</body>
